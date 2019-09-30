039248d8678484760ef644c9085d5ffb321e7b23
taneba <taneba.vla@gmail.com>
2019-09-30 15:26:46 +0900

    misc

---
+++ pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactLogo from '../../images/react.svg'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Next.js boilerplate</h1>
+      <div>
+        <ReactLogo css="width: 40px;" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
