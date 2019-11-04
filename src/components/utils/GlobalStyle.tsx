1a9c973b2fceb2f16b4c6af9d1a4758cceeade08
taneba <taneba.vla@gmail.com>
2019-11-05 02:15:52 +0900

    minor adjustment of  globalstyle

--- src/components/utils/GlobalStyle.tsx
+++ src/components/utils/GlobalStyle.tsx
@@ -110,6 +110,16 @@ const GlobalStyle = createGlobalStyle`
   *::after {
     box-sizing: border-box;
   }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+
+  #__next {
+    width: 100%;
+    height: 100%;
+  }
 `
 
 export default GlobalStyle
